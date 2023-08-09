0e5b5346dcfdf70e86239f248d3fd171ad4660ec
WilSotoA <113364120+WilSotoA@users.noreply.github.com>
2023-08-09 18:57:24 -0500

    special method and exercises

--- POO/metodos_especiales.py
+++ POO/metodos_especiales.py
@@ -1,4 +1,28 @@
 class Persona:
-    def __init__(self, nombre, edad ):
+    def __init__(self, nombre, edad):
         self.nombre = nombre
         self.edad = edad
+
+    def __str__(self):
+        return f"Persona(nombre={self.nombre},edad={self.edad})"
+
+    def __repr__(self):
+        return f'Persona("{self.nombre}","{self.edad}")'
+
+    def __add__(self, otro):
+        nuevo_valor = self.edad + otro.edad
+        return Persona(self.nombre + otro.nombre, nuevo_valor)
+
+
+wilmer = Persona("Wilmer", 19)
+andres = Persona("Pedro", 30)
+maria = Persona("Pedro", 20)
+
+nueva_persona = wilmer + andres + maria
+
+print(nueva_persona.edad)
+
+# repre = repr(wilmer)
+# resultado = eval(repre)
+
+# print(resultado.nombre)
